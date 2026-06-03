65309df633d5bf216a977db201a27d8ecde59b71
AnimeshA867 <animeshacharya867+github@gmail.com>
2026-06-03 14:02:58 +0545

    Added LivenessProbe to the deployment file

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.get("/about", (req, res) => {
   res.send("This is the about page.");
 });
